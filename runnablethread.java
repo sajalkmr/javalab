78c46ac7d17f781a0e3cfb2cb5a9dcce33bf255e
Sajal <sajal.kumar2003@gmail.com>
2024-03-02 08:47:10 +0530

    Update runnablethread.java

--- runnablethread.java
+++ runnablethread.java
@@ -10,7 +10,7 @@ class MyRunnable implements Runnable {
         try {
             for (int i = 1; i <= 5; i++) {
                 System.out.println("Thread " + threadName + ": Count - " + i);
-                Thread.sleep(500); // Suspend the thread for 500 milliseconds
+                Thread.sleep(500); 
             }
         } catch (InterruptedException e) {
             System.out.println("Thread " + threadName + " interrupted.");
@@ -22,14 +22,14 @@ class MyRunnable implements Runnable {
 public class RunnableThreadDemo {
     public static void main(String[] args) {
         System.out.println("Main thread starting.");
-        // Create and start two threads
+        
         Thread thread1 = new Thread(new MyRunnable("Thread 1"));
         Thread thread2 = new Thread(new MyRunnable("Thread 2"));
-        thread1.start(); // Start thread1
-        thread2.start(); // Start thread2
+        thread1.start(); 
+        thread2.start(); 
         try {
-            thread1.join(); // Wait for thread1 to finish
-            thread2.join(); // Wait for thread2 to finish
+            thread1.join(); 
+            thread2.join(); 
         } catch (InterruptedException e) {
             System.out.println("Main thread interrupted.");
         }
