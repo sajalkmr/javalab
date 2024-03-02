63240a08f202a281a017a7864717b8c4317b3c35
Sajal <sajal.kumar2003@gmail.com>
2024-03-02 08:42:02 +0530

    Update concurrency.java

--- concurrency.java
+++ concurrency.java
@@ -1,7 +1,7 @@
 class MyThread extends Thread {
     public MyThread(String name) {
-        super(name); // Call base class constructor
-        start(); // Start the thread
+        super(name); 
+        start(); 
     }
 
     @Override
@@ -9,7 +9,7 @@ class MyThread extends Thread {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Thread " + getName() + ": Count - " + i);
             try {
-                sleep(500); // Suspend the thread for 500 milliseconds
+                sleep(500); 
             } catch (InterruptedException e) {
                 System.out.println("Thread " + getName() + " interrupted.");
             }
@@ -21,14 +21,14 @@ class MyThread extends Thread {
 public class MyThreadDemo {
     public static void main(String[] args) {
         System.out.println("Main thread starting.");
-        // Create and start a child thread
+        
         MyThread childThread = new MyThread("Child Thread");
 
-        // Main thread execution
+        
         for (int i = 1; i <= 5; i++) {
             System.out.println("Main Thread: Count - " + i);
             try {
-                Thread.sleep(1000); // Suspend the main thread for 1000 milliseconds
+                Thread.sleep(1000); 
             } catch (InterruptedException e) {
                 System.out.println("Main thread interrupted.");
             }
